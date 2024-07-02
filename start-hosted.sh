133c3fc05f4f76a5d207f9523061aa3d6d4f7eac
Shane Baldacchino <shane@baldacchino.net>
2024-07-03 09:54:25 +1000

    Delete start-hosted.sh

--- start-hosted.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# "rtsp://aws:summit2024@10.0.0.2:554/Streaming/Channels/101/"
-python3 app.py \
-  -d CPU \
-  -i  "rtsp://aws:summit2024@10.0.0.2:554/Streaming/Channels/101/" \
-  -m ./object_detection/ssdlite_mobilenet_v2/FP32/ssdlite_mobilenet_v2.xml \
-  -at ssd
